5e54d55a9977197caf5ca1150c323055681f4277
SultanRyaz <sryazyapov@bk.ru>
2023-10-09 20:32:37 +0300

    Update Tic-Tac-Game.py

--- Tic-Tac-Game.py
+++ Tic-Tac-Game.py
@@ -11,10 +11,3 @@ def print_scheme():
     print(scheme[7], end=" ")
     print(scheme[8])
 scheme = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-end_game = False
-player1 = input('Выберите, чем будет ходить игрок 1: ')
-if player1 == "X":
-    players = ["X", "0"]
-if player1 == "0":
-    players = ["0", "X"]
